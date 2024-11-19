59c40905b6c48a6952e52f9733ed4b132844e981
Svetlana Soboleva <132843403+svetlana-soboleva@users.noreply.github.com>
2024-11-19 19:07:49 +0100

    eat: implement StoryPartsCarousel with speech synthesis functionality
    text-to-speech synthesis for individual words on hover
    
    the speech synthesis logic is embedded in the component. Upcoming refactor will modularize this functionality for better scalability and readability

--- frontend/src/components/stories/StoryPage.tsx
+++ frontend/src/components/stories/StoryPage.tsx
@@ -33,8 +33,8 @@ export const StoryPage = ({ story, isLoading }: Props) => {
           onLoad={() => setImageLoaded(true)}
         />
       </figure>
-      <div className="card-body lg:w-1/2 h-1/2 lg:h-full flex flex-col justify-center md:justify-between lg:justify-between gap-4 lg:m-4 items-center text-center">
-        <h2 className="card-title text-2xl md:text-4xl text-purple-500 lg:text-4xl lg:mt-8">
+      <div className="card-body w-96 flex flex-col">
+        <h2 className="card-title  text-purple-500">
           {story.title.replace(/([A-Z])/g, " $1").trim()}
         </h2>
         <StoryPartsCarousel chunks={story.chunks} />
--- frontend/src/components/stories/StoryPartsCarousel.tsx
+++ frontend/src/components/stories/StoryPartsCarousel.tsx
@@ -1,19 +1,74 @@
 import { Chunk } from "@/data/types/types";
+import { useEffect, useState } from "react";
+
 interface Props {
   chunks: Chunk[];
 }
 
 export const StoryPartsCarousel = ({ chunks }: Props) => {
+  const [voices, setVoices] = useState<SpeechSynthesisVoice[]>([]);
+  const synth = window.speechSynthesis;
 
-  
+  useEffect(() => {
+    const loadVoices = () => {
+      const availableVoices = synth.getVoices();
+      setVoices(availableVoices);
+    };
+
+    loadVoices();
+    if (synth.onvoiceschanged !== undefined) {
+      synth.onvoiceschanged = loadVoices;
+    }
+  }, [synth]);
+
+  const speakWord = (word: string) => {
+    if (synth.speaking) {
+      console.warn("Already speaking!");
+      return;
+    }
+
+    if (word !== "") {
+      const speakText = new SpeechSynthesisUtterance(word);
+
+      const englishVoice =
+        voices.find((voice) => voice.lang.startsWith("en")) || voices[0];
+
+      speakText.voice = englishVoice;
+      speakText.rate = 1;
+      speakText.pitch = 1;
+
+      speakText.onend = () => console.log("Speech finished");
+      speakText.onerror = () => console.error("Speech error");
+
+      synth.speak(speakText);
+    }
+  };
+
   return (
-    <div className="carousel carousel-center bg-neutral rounded-box space-x-6">
-      {chunks.map((chunk, index) => (
-        <div className="carousel-item w-full flex flex-col">
-          <p className="text-md uppercase leading-10 text-center lg:p-8 lg:text-md xl:text-xl">{chunk.text}</p>
-          <p className="text-sm font-bold text-right px-4">{index + 1} - {chunks.length}</p>
-        </div>
-      ))}
+    <div className="carousel carousel-center h-52 bg-neutral rounded-box space-x-6 flex flex-row flex-wrap">
+      {chunks.map((chunk, index) => {
+        const words = chunk.text.split(" ");
+        return (
+          <div key={index} className="carousel-item w-full h-full ">
+            <div className="uppercase flex flex-wrap">
+              {words.map((word, wordIndex) => (
+                <div>
+                  <span
+                    key={wordIndex}
+                    className="cursor-pointer hover:bg-gray-500 rounded-md p-1 hover:text-white"
+                    onMouseEnter={() => speakWord(word)}
+                  >
+                    {word}
+                  </span>
+                </div>
+              ))}
+              <p className="text-sm font-bold text-right px-4">
+                {index + 1} - {chunks.length}
+              </p>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
