88e185f0259e6c8f1e94891b3b01ae24ebd40c68
Svetlana Soboleva <132843403+svetlana-soboleva@users.noreply.github.com>
2024-11-22 08:45:38 +0100

    update navigation to signin

--- frontend/src/components/welcome/WelcomeMsg.tsx
+++ frontend/src/components/welcome/WelcomeMsg.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "@tanstack/react-router";
+import { useNavigate } from "@tanstack/react-router";
 
 export const WelcomeMsg = () => {
   const navigate = useNavigate();
